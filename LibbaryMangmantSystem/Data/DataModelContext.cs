2170bf374f699cb919e2ba935e289adb1325bb18
KamolHasanov <khasanovkamol3834@gmail.com>
2021-04-14 17:40:19 +0500

    Add project files.

---
+++ LibbaryMangmantSystem/Data/DataModelContext.cs
@@ -0,0 +1,21 @@
+﻿using LibbaryMangmantSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibbaryMangmantSystem.Data
+{
+    public class DataModelContext: IdentityDbContext
+    {
+        public DataModelContext(DbContextOptions<DataModelContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
---
+++ LibbaryMangmantSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LibbaryMangmantSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
